e8806a2a627cdc7e799f7dfe61d9dc3e9ce55be9
hxtesclre <157316933+hxtesclre@users.noreply.github.com>
2024-01-26 11:20:35 +0300

    Update script.lua

--- script.lua
+++ script.lua
@@ -5,11 +5,10 @@
     -- Настраиваем GUI
     screenGui.Name = "CoordinateGui"
     textLabel.Name = "CoordinateLabel"
-    textLabel.Size = UDim2.new(0, 200, 0.2, 50)
+    textLabel.Size = UDim2.new(0, 200, 0.5, 50)
     textLabel.Position = UDim2.new(0, 10, 0, 10)
     textLabel.Text = "Coordinates: "
     textLabel.BackgroundColor3 = Color3.new(4, 28, 50) -- Задаем цвет фона (красный в данном случае)
-    textLabel.BorderSizePixel = 0 -- Убираем границу
     textLabel.TextColor3 = Color3.new(1, 1, 1) -- Задаем цвет текста (белый в данном случае)
 
     -- Создаем закругление
