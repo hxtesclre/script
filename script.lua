7afc73eba777c2c42393a34bc3161b4fc81f0dec
hxtesclre <157316933+hxtesclre@users.noreply.github.com>
2024-01-26 11:24:34 +0300

    Update script.lua

--- script.lua
+++ script.lua
@@ -8,9 +8,9 @@
     textLabel.Size = UDim2.new(0, 200, 0.2, 50)
     textLabel.Position = UDim2.new(0, 10, 0, 10)
     textLabel.Text = "Coordinates: "
-    textLabel.BackgroundColor3 = Color3.new(1, 0, 0) -- Задаем цвет фона (красный в данном случае)
+    textLabel.BackgroundColor3 = Color3.fromRGB(4, 28, 50) -- Задаем цвет фона (красный в данном случае)
     textLabel.BorderSizePixel = 0 -- Убираем границу
-    textLabel.TextColor3 = Color3.new(4, 28, 50) -- Задаем цвет текста (белый в данном случае)
+    textLabel.TextColor3 = Color3.new(1, 1, 1) -- Задаем цвет текста (белый в данном случае)
 
     -- Создаем закругление
     local corner = Instance.new("UICorner")
