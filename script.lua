c73de1e6460d36664d2706b2e8079f89020eaca7
hxtesclre <157316933+hxtesclre@users.noreply.github.com>
2024-01-26 11:17:41 +0300

    Update script.lua

--- script.lua
+++ script.lua
@@ -5,22 +5,17 @@
     -- Настраиваем GUI
     screenGui.Name = "CoordinateGui"
     textLabel.Name = "CoordinateLabel"
-    textLabel.Size = UDim2.new(0, 200, 0, 50)
+    textLabel.Size = UDim2.new(0, 200, 0.2, 50)
     textLabel.Position = UDim2.new(0, 10, 0, 10)
     textLabel.Text = "Coordinates: "
-    textLabel.BackgroundColor3 = Color3.new(1, 0, 0) -- Задаем цвет фона (красный в данном случае)
+    textLabel.BackgroundColor3 = Color3.new(4, 28, 50) -- Задаем цвет фона (красный в данном случае)
     textLabel.BorderSizePixel = 0 -- Убираем границу
-    textLabel.TextColor3 = Color3.new(4, 28, 50) -- Задаем цвет текста (белый в данном случае)
+    textLabel.TextColor3 = Color3.new(1,1,1) -- Задаем цвет текста (белый в данном случае)
 
     -- Создаем закругление
     local corner = Instance.new("UICorner")
     corner.CornerRadius = UDim.new(0, 20) -- Задаем радиус закругления
     corner.Parent = textLabel -- Добавляем закругление к нашему GUI
-
-    -- Добавляем масштабирование
-    local scale = Instance.new("UIScale")
-    scale.Scale = 1 -- Задаем масштаб (1.5 в данном случае)
-    scale.Parent = screenGui -- Добавляем масштабирование к нашему GUI
 
     -- Добавляем GUI в игру
     screenGui.Parent = playerGui
