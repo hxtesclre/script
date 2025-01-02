dfdc99ecde65210a780ed5b1c8df826debacf560
hxtesclre <157316933+hxtesclre@users.noreply.github.com>
2025-01-02 22:25:15 +0300

    Update tower_data.lua

--- tower_data.lua
+++ tower_data.lua
@@ -19,7 +19,17 @@ local towerConfigurations = {
             -- Добавьте сюда другие дополнения для Caveman
         }
     },
-    -- Добавьте сюда другие башни
+    ["IceBishop"] = {
+        basePrice = {Coins = 90, Gems = 300},
+        suffixes = {
+            ["EternalGlore"] = {Coins = 1000, Gems = 2000},
+            ["LightningSpeed"] = {Coins = 95, Gems = 400},
+            ["FarReach1"] = {Coins = 200, Gems = 400},
+            ["FarReach2"] = {Coins = 250, Gems = 500},
+            ["FarReach3"] = {Coins = 300, Gems = 600},
+            -- Добавьте сюда другие дополнения для Caveman
+        }
+    },
 }
 
 return towerConfigurations
